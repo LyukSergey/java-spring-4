a9d2c57f7485228fc3d05cdc92acec5e878e1c53
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-07-08 09:53:17 +0300

    L1-spring-boot-demo 1st commit

---
+++ L1-spring-boot-demo/src/main/java/com/lss/l1springbootdemo/L1SpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l1springbootdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1SpringBootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1SpringBootDemoApplication.class, args);
+    }
+
+}
