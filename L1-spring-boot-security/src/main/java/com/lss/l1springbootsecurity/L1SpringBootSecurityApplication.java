d6841664989adda4e5a46247142598dfd6cb8cf5
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-07-08 18:58:57 +0300

    L1-spring-boot-security

---
+++ L1-spring-boot-security/src/main/java/com/lss/l1springbootsecurity/L1SpringBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.lss.l1springbootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1SpringBootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1SpringBootSecurityApplication.class, args);
+    }
+
+}
---
+++ L1-spring-boot-security/src/test/java/com/lss/l1springbootsecurity/L1SpringBootSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.l1springbootsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1SpringBootSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
