013edd747dce9144ab4c007202d395092c5c345c
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-07-09 22:45:02 +0300

    L1-spring-boot-security-db

---
+++ l1-spring-security-db/src/main/java/com/lss/l1springsecuritydb/testData/DataInitializer.java
@@ -0,0 +1,25 @@
+package com.lss.l1springsecuritydb.testData;
+
+import com.lss.l1springsecuritydb.entity.User;
+import com.lss.l1springsecuritydb.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) throws Exception {
+        User adminUser = new User();
+        adminUser.setUsername("admin");
+        adminUser.setPassword(passwordEncoder.encode("password")); // Шифруємо пароль
+        adminUser.setRole("ROLE_ADMIN"); // Ролі мають починатися з "ROLE_"
+        userRepository.save(adminUser);
+    }
+}
