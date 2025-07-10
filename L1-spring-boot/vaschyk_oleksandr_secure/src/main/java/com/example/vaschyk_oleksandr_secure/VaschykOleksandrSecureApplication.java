5ff7cd6a5b48f447bc5519a08e355c9cdc5f60de
Oleksandr Vaschyk <vashil047@gmail.com>
2025-07-10 14:20:12 +0300

    upd

---
+++ L1-spring-boot/vaschyk_oleksandr_secure/src/main/java/com/example/vaschyk_oleksandr_secure/VaschykOleksandrSecureApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaschyk_oleksandr_secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaschykOleksandrSecureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VaschykOleksandrSecureApplication.class, args);
+    }
+
+}
---
+++ L1-spring-boot/vaschyk_oleksandr_secure/src/test/java/com/example/vaschyk_oleksandr_secure/VaschykOleksandrSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaschyk_oleksandr_secure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaschykOleksandrSecureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
