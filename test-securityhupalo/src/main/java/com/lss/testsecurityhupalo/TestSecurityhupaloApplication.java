cc3208facd7a7a36488cddef0636ec25a08909d4
yaroslav_hupalo <rusr9187@gmail.com>
2025-07-11 15:14:22 +0300

    .

---
+++ test-securityhupalo/src/main/java/com/lss/testsecurityhupalo/TestSecurityhupaloApplication.java
@@ -0,0 +1,13 @@
+package com.lss.testsecurityhupalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSecurityhupaloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSecurityhupaloApplication.class, args);
+    }
+
+}
---
+++ test-securityhupalo/src/test/java/com/lss/testsecurityhupalo/TestSecurityhupaloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lss.testsecurityhupalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSecurityhupaloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
