e5ef364d71ed0a95db8f58b69fc79ccc4c971614
angru <angruhahetun@gmail.com>
2025-07-11 10:28:06 +0300

    1

--- L1-spring-boot/src/main/java/com/example/demo/service/BankManagementService.java
+++ L1-spring-boot/src/main/java/com/example/demo/service/BankManagementService.java
@@ -1,10 +1,23 @@
 package com.example.demo.service;
-
+import com.example.demo.repository.UserRepository;
+import java.util.List;
 import com.example.demo.dto.UserDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 public interface BankManagementService {
 
     UserDto registerNewUser(String name, String surname, Long bankId);
 
     void deleteUser(Long userId);
+}
+
+
+@Service
+@RequiredArgsConstructor
+public class BankManagementService {
+    public List<com.example.demo.service.User> getUsersByBank(Long bankId) {
+        UserRepository userRepository;
+        return userRepository.findAllByBankId(bankId);
+    }
 }
