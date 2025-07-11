62e276ede324fae6065f4b71390079cb716741bc
angru <angruhahetun@gmail.com>
2025-07-11 10:28:59 +0300

    2

---
+++ L1-hetun/src/main/java/com/example/l1hetun/L1HetunApplication.java
@@ -0,0 +1,13 @@
+package com.example.l1hetun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1HetunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1HetunApplication.class, args);
+    }
+
+}
---
+++ L1-hetun/src/test/java/com/example/l1hetun/L1HetunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l1hetun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1HetunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
