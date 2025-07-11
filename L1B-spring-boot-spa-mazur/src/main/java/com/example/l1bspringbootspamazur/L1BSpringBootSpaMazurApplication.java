8c316b63d6ea3334f0c1db2662c7f4d85a63bce6
Sviatozar <switozarmazur@gmail.com>
2025-07-11 09:42:38 +0300

    Spring Security Task

---
+++ L1B-spring-boot-spa-mazur/src/main/java/com/example/l1bspringbootspamazur/L1BSpringBootSpaMazurApplication.java
@@ -0,0 +1,13 @@
+package com.example.l1bspringbootspamazur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1BSpringBootSpaMazurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(L1BSpringBootSpaMazurApplication.class, args);
+	}
+
+}
---
+++ L1B-spring-boot-spa-mazur/src/test/java/com/example/l1bspringbootspamazur/L1BSpringBootSpaMazurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l1bspringbootspamazur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1BSpringBootSpaMazurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
