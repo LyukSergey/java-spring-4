629e0391bb40bc3a6c67f74210b07a900d003b57
edikg <edikgvildis@gmail.com>
2025-07-11 10:22:22 +0300

    1

--- L1-spring-boot/src/main/java/com/example/demo/service/BankManagementService.java
+++ L1-spring-boot/src/main/java/com/example/demo/service/BankManagementService.java
@@ -7,4 +7,13 @@ public interface BankManagementService {
     UserDto registerNewUser(String name, String surname, Long bankId);
 
     void deleteUser(Long userId);
+
+    List<UserDto> getUsersByBank(Long bankId);
+    @Override
+    public List<UserDto> getUsersByBank(Long bankId) {
+        final List<User> users = userRepository.findAllByBankId(bankId);
+        return users.stream()
+                .map(userMapper::toDto)
+                .toList();
+    }
 }
