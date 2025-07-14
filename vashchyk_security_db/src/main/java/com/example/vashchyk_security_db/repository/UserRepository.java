0a9b8443f257091fa58292c002eb8c8c43040b90
Oleksandr Vaschyk <vashil047@gmail.com>
2025-07-14 10:02:35 +0300

    upd pz db 1

--- vashchyk_security_db/src/main/java/com/example/vashchyk_security_db/repository/UserRepository.java
+++ vashchyk_security_db/src/main/java/com/example/vashchyk_security_db/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.vashchyk_security_db.repository;
 
 import com.example.vashchyk_security_db.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
