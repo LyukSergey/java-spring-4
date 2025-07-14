a74213f5e35e75af892da1c8463ecc8f0008a8cf
Khomenko <andreiy.khomenko@vit.edu.ua>
2025-07-14 11:39:39 +0300

    1

---
+++ L1-spring-boot-security-spa/L1-sb-profilling-starter-homenko/src/main/java/org/example/l1sbprofillingstarterhomenko/L1SbProfillingStarterHomenkoApplication.java
@@ -0,0 +1,13 @@
+package org.example.l1sbprofillingstarterhomenko;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1SbProfillingStarterHomenkoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1SbProfillingStarterHomenkoApplication.class, args);
+    }
+
+}
