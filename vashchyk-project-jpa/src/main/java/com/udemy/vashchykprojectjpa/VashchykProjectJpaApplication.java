3fb84eba62c425682a25f45e370df4a2232aa63e
Oleksandr Vaschyk <vashil047@gmail.com>
2025-07-15 21:10:22 +0300

    dell bugs + commit1

---
+++ vashchyk-project-jpa/src/main/java/com/udemy/vashchykprojectjpa/VashchykProjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.vashchykprojectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VashchykProjectJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VashchykProjectJpaApplication.class, args);
+	}
+
+}
---
+++ vashchyk-project-jpa/src/test/java/com/udemy/vashchykprojectjpa/VashchykProjectJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.vashchykprojectjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VashchykProjectJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
