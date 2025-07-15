3240cf487e4e31b0f55b6e9fe436da6ca2fefaae
Oleksandr Vaschyk <vashil047@gmail.com>
2025-07-15 22:01:14 +0300

    upd jpa 2.o

---
+++ vashchyk-data-project-jpa/src/main/java/com/udemy/vashchykdataprojectjpa/VashchykDataProjectJpaApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.vashchykdataprojectjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VashchykDataProjectJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VashchykDataProjectJpaApplication.class, args);
+    }
+
+}
