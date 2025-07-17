16f66211bf01b77410c556afeee4c24362408d8a
Oleksandr Vaschyk <vashil047@gmail.com>
2025-07-17 10:25:50 +0300

    upd zalik

--- L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/repository/EmployeeRepository.java
+++ L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.lss.l1bzalic_303_304.repository;
 import com.lss.l1bzalic_303_304.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import java.util.Optional;
 
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
 }
