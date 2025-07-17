cfd15ea96c446800426c1449d763e450cd480011
Sviatozar <switozarmazur@gmail.com>
2025-07-17 12:28:59 +0300

    Spring Task Addition

---
+++ L1-spring-boot-security-spa-add-new-user-mazur/src/main/java/com/example/l1springbootsecurityspaaddnewusermazur/L1SpringBootSecuritySpaAddNewUserMazurApplication.java
@@ -0,0 +1,13 @@
+package com.example.l1springbootsecurityspaaddnewusermazur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class L1SpringBootSecuritySpaAddNewUserMazurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(L1SpringBootSecuritySpaAddNewUserMazurApplication.class, args);
+    }
+
+}
---
+++ L1-spring-boot-security-spa-add-new-user-mazur/src/test/java/com/example/l1springbootsecurityspaaddnewusermazur/L1SpringBootSecuritySpaAddNewUserMazurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.l1springbootsecurityspaaddnewusermazur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class L1SpringBootSecuritySpaAddNewUserMazurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
