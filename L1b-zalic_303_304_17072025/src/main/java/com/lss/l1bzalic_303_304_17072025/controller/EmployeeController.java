9f9f7bd38a66bc87944f2535b1bb169cad121c3d
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-07-17 22:19:21 +0300

    L1b-zalic_303_304_17072025

--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/EmployeeController.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/EmployeeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/employees")
+@RequestMapping
 @RequiredArgsConstructor
 public class EmployeeController {
 
