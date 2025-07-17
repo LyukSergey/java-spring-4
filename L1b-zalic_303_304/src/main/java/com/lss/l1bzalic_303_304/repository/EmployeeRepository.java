95449ba260c0348ddf5e4a2f1d7fb131a713a1d9
Home <mahfeujc@gmail.com>
2025-07-17 21:51:30 +0300

    zalik-test3

--- L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/repository/EmployeeRepository.java
+++ L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/repository/EmployeeRepository.java
@@ -3,6 +3,12 @@ package com.lss.l1bzalic_303_304.repository;
 import com.lss.l1bzalic_303_304.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
+import java.util.Optional;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+
+    Collection<Object> findTop3ByOrderBySalaryDesc();
 
 }
