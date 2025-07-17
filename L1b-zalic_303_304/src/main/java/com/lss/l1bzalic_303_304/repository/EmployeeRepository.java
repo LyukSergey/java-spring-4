3ea5ac1024429b293edffad7a795b0ac94c3e0cf
first-cerberus <sans15365@gmail.com>
2025-07-17 09:21:10 +0300

    zalik selyamiev

--- L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/repository/EmployeeRepository.java
+++ L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/repository/EmployeeRepository.java
@@ -3,6 +3,8 @@ package com.lss.l1bzalic_303_304.repository;
 import com.lss.l1bzalic_303_304.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+import java.util.List;
 
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByLastNameStartingWith(String prefix);
 }
---
+++ L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,31 @@
+package com.lss.l1bzalic_303_304.service.impl;
+
+import com.lss.l1bzalic_303_304.dto.EmployeeDto;
+import com.lss.l1bzalic_303_304.repository.EmployeeRepository;
+import com.lss.l1bzalic_303_304.service.EmployeeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    @Override
+    public List<EmployeeDto> findByLastNameStartsWith(String prefix) {
+        return employeeRepository.findByLastNameStartingWith(prefix).stream()
+                .map(emp -> new EmployeeDto(
+                        emp.getId(),
+                        emp.getLastName() + " " + emp.getFirstName(),
+                        emp.getPosition(),
+                        emp.getSalary(),
+                        emp.getDepartment().getName()
+                ))
+                .toList();
+    }
+
+
+}
