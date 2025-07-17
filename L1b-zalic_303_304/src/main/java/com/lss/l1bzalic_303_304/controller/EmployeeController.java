162dacb782eaea5603e7fd3739534b30ac9c8618
edikg <edikgvildis@gmail.com>
2025-07-17 09:25:19 +0300

    1

---
+++ L1b-zalic_303_304/src/main/java/com/lss/l1bzalic_303_304/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.lss.l1bzalic_303_304.controller;
+
+import com.lss.l1bzalic_303_304.dto.EmployeeDto;
+import com.lss.l1bzalic_303_304.service.impl.EmployeeService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/search/top-3-by-salary")
+    public List<EmployeeDto> getTop3EmployeesBySalary() {
+        return employeeService.getTop3BySalary();
+    }
+}
