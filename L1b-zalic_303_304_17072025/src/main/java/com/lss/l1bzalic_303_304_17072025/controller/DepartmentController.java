94a12cd35564018758879cf175a65839a55c89f2
MrVadmen <mr.vadmen@gmail.com>
2025-07-17 18:29:17 -1200

    zalik Palamarchuk V!!! Variant 16

--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/DepartmentController.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/DepartmentController.java
@@ -2,16 +2,24 @@ package com.lss.l1bzalic_303_304_17072025.controller;
 
 import com.lss.l1bzalic_303_304_17072025.dto.DepartmentDto;
 import com.lss.l1bzalic_303_304_17072025.service.DepartmentService;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
-@RequestMapping
-@RequiredArgsConstructor
+@RequestMapping("/departments")
 public class DepartmentController {
 
+    private final DepartmentService departmentService;
+
+    public DepartmentController(DepartmentService departmentService) {
+        this.departmentService = departmentService;
+    }
+
+    @GetMapping
+    public List<DepartmentDto> getAllDepartmentsSortedByName() {
+        return departmentService.getAllDepartmentsSortedByName();
+    }
 }
--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/DepartmentServiceImpl.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/DepartmentServiceImpl.java
@@ -1,14 +1,28 @@
 package com.lss.l1bzalic_303_304_17072025.service.impl;
 
 import com.lss.l1bzalic_303_304_17072025.dto.DepartmentDto;
+import com.lss.l1bzalic_303_304_17072025.entity.Department;
 import com.lss.l1bzalic_303_304_17072025.repository.DepartmentRepository;
 import com.lss.l1bzalic_303_304_17072025.service.DepartmentService;
-import java.util.List;
-import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
-@RequiredArgsConstructor
 public class DepartmentServiceImpl implements DepartmentService {
 
+    private final DepartmentRepository departmentRepository;
+
+    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+        this.departmentRepository = departmentRepository;
+    }
+
+    @Override
+    public List<DepartmentDto> getAllDepartmentsSortedByName() {
+        List<Department> departments = departmentRepository.findAllByOrderByNameAsc();
+        return departments.stream()
+                .map(dep -> new DepartmentDto(dep.getId(), dep.getName()))
+                .collect(Collectors.toList());
+    }
 }
