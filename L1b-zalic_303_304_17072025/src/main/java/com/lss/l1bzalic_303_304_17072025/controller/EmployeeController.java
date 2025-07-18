792d0671a91bc2c11d386a2ecf3b0ce86b46ef6c
Horov4ik <horov4ik@gmail.com>
2025-07-18 10:08:15 +0300

    zalik

--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/EmployeeController.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/EmployeeController.java
@@ -6,11 +6,17 @@ import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
+@RequestMapping("/employees")
 @RequiredArgsConstructor
 public class EmployeeController {
+    private final EmployeeService employeeService;
 
+    @GetMapping("/search/by-name")
+    public List<EmployeeDto> searchEmployeesByName(@RequestParam("name") String name) {
+        return employeeService.searchEmployeesByName(name);
+    }
 }
