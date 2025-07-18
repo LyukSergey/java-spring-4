688cbbd53d8f7db69316fe4c2b0375730a1f1678
scr1mer <admin.ua@icloud.com>
2025-07-18 15:10:25 +0300

    zal1k na 5

--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/EmployeeController.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/controller/EmployeeController.java
@@ -9,8 +9,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping
-@RequiredArgsConstructor
+@RequestMapping("/employees")
+
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<EmployeeDto> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
 
 }
--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/EmployeeServiceImpl.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/EmployeeServiceImpl.java
@@ -1,11 +1,48 @@
 package com.lss.l1bzalic_303_304_17072025.service.impl;
 
+import com.lss.l1bzalic_303_304_17072025.dto.EmployeeDto;
+import com.lss.l1bzalic_303_304_17072025.entity.Employee;
+import com.lss.l1bzalic_303_304_17072025.repository.EmployeeRepository;
 import com.lss.l1bzalic_303_304_17072025.service.EmployeeService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
 @Service
-@RequiredArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+
+
+    private EmployeeDto convertToDto(Employee employee) {
+        String fullName = employee.getFirstName() + " " + employee.getLastName();
+        String position = employee.getPosition();
+        BigDecimal salary = employee.getSalary();
+        String departmentName = employee.getDepartment() != null ? employee.getDepartment().getName() : null;
+
+        return new EmployeeDto(
+                employee.getId(),
+                fullName,
+                position,
+                salary,
+                departmentName
+        );
+    }
+
+    @Transactional
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        return employeeRepository.findAll().stream()
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+    }
 
 }
