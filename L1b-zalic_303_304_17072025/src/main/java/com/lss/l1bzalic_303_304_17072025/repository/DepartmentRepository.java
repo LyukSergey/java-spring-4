50f5cfd426bd7417816ec3516badddf38538431d
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-07-18 21:11:28 +0300

    Add explanation about mistake

--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/repository/DepartmentRepository.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/repository/DepartmentRepository.java
@@ -1,13 +1,18 @@
 package com.lss.l1bzalic_303_304_17072025.repository;
 
 import com.lss.l1bzalic_303_304_17072025.entity.Department;
+import com.lss.l1bzalic_303_304_17072025.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import org.springframework.data.repository.query.Param;
 
 public interface DepartmentRepository extends JpaRepository<Department, Long> {
-    @Query("SELECT d FROM Department d LEFT JOIN FETCH d.employees")
-    List<Department> findAllWithEmployees();
+    //LEFT JOIN FETCH d.employees - тут лишнє
+//    @Query("SELECT d FROM Department d LEFT JOIN FETCH d.employees")
+    //Якщо ти вирішив йти через BD, то треба було зробити так
+    @Query("SELECT d FROM Department d JOIN d.employees e GROUP BY d.id HAVING COUNT(e) > :count")
+    List<Department> findAllWithEmployees(@Param("count") int count);
 }
 
--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/DepartmentServiceImpl.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/DepartmentServiceImpl.java
@@ -3,6 +3,7 @@ package com.lss.l1bzalic_303_304_17072025.service.impl;
 import com.lss.l1bzalic_303_304_17072025.dto.DepartmentDto;
 import com.lss.l1bzalic_303_304_17072025.repository.DepartmentRepository;
 import com.lss.l1bzalic_303_304_17072025.service.DepartmentService;
+import jakarta.transaction.Transactional;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,10 +13,11 @@ import org.springframework.stereotype.Service;
 public class DepartmentServiceImpl implements DepartmentService {
     private final DepartmentRepository departmentRepository;
 
+    //Треба було використати @Transactional
     @Override
     public List<DepartmentDto> findDepartmentsWithEmployeesMoreThan(int count) {
-        return departmentRepository.findAllWithEmployees().stream()
-            .filter(dep -> dep.getEmployees() != null && dep.getEmployees().size() > count)
+        return departmentRepository.findAllWithEmployees(count).stream()
+//            .filter(dep -> dep.getEmployees() != null && dep.getEmployees().size() > count)
             .map(dep -> new DepartmentDto(dep.getId(), dep.getName()))
             .toList();
     }
