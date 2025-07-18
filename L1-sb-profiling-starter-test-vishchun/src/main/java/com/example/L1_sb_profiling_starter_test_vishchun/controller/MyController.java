fdf72d168abdc1fdc93fed48f03485681475f5d5
WhyDie <20050113vishun@gmail.com>
2025-07-18 08:03:06 +0300

    vishchun

--- L1-sb-profiling-starter-test-vishchun/src/main/java/com/example/L1_sb_profiling_starter_test_vishchun/controller/MyController.java
+++ L1-sb-profiling-starter-test-vishchun/src/main/java/com/example/L1_sb_profiling_starter_test_vishchun/controller/MyController.java
@@ -1,4 +1,5 @@
 package com.example.L1_sb_profiling_starter_test_vishchun.controller;
+
 import com.example.L1_sb_profiling_starter_test_vishchun.service.MyService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -7,14 +8,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/do")
+@RequestMapping("/do") // Base path for all endpoints
 @RequiredArgsConstructor
 public class MyController {
     private final MyService myService;
-    // ...existing code...
+
     @GetMapping
     public ResponseEntity<Void> getUsers() {
         myService.doWork();
         return ResponseEntity.noContent().build();
     }
+    
 }
