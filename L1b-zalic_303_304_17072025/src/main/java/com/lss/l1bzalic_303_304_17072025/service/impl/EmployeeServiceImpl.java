4d8fac3c6d42f7dd7d656a479bc25dd9778e2432
sergijluk <Sergii.Lyuk@externals.adidas.com>
2025-07-19 10:46:37 +0300

    Add explanation about mistake

--- L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/EmployeeServiceImpl.java
+++ L1b-zalic_303_304_17072025/src/main/java/com/lss/l1bzalic_303_304_17072025/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.lss.l1bzalic_303_304_17072025.dto.EmployeeDto;
 import com.lss.l1bzalic_303_304_17072025.repository.EmployeeRepository;
 import com.lss.l1bzalic_303_304_17072025.service.EmployeeService;
 import com.lss.l1bzalic_303_304_17072025.dto.EmployeeDto;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +17,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private final EmployeeRepository employeeRepository;
 
+    //@Transactional обовязково. Тому буде org.hibernate.LazyInitializationException
     @Override
+    @Transactional
     public List<EmployeeDto> searchByName(String name) {
         return employeeRepository
                 .findByFirstNameContainingIgnoreCaseOrLastNameContainingIgnoreCase(name, name)
